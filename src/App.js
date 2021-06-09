fcd2c1d6a23c2002f33b4002f8f2441b5b8bfbd9
Danilo <danilo.rocha1@uni9.edu.br>
2021-06-08 23:12:38 -0300

    Too many changes to describe :b

--- src/App.js
+++ src/App.js
@@ -1,9 +1,77 @@
+import { useState } from "react";
+import { Button, TextField } from "@material-ui/core";
+import { createGame, sendGuess } from "./utils/api";
+import Hangman from './components/Hangman';
+
 function App() {
-  return (
-    <div className="App">
-      TODO: create view
-    </div>
-  );
+    const [target, setTarget] = useState('');
+    const [guess, setGuess] = useState('');
+    const [token, setToken] = useState('');
+
+    const handleCreateGame = () => {
+        createGame(setTarget, setToken);
+    };
+
+    const handleInputChange = (e) => {
+        const { value } = e.target;
+        if (value.match(/^[a-zA-Z ]{0,1}$/)) {
+            setGuess(value);
+        }
+    }
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        if (guess !== '' && guess !== ' ') {
+            // TODO: send guess to API
+            console.log(guess);
+            sendGuess(token, guess);
+        }
+    }
+
+    return (
+        <div className="App" style={styles.app}>
+            <div className="hangman" style={styles.container}>
+                <Hangman target={target} />
+            </div>
+            <div className='game-controls' style={styles.container}>
+                {target ?
+                    <form noValidate autoComplete='off' onSubmit={handleFormSubmit}>
+                        <TextField 
+                            label='Guess a letter'
+                            variant='outlined'
+                            value={guess}
+                            onChange={handleInputChange}
+                        />
+                    </form>
+                :
+                    <Button onClick={handleCreateGame} variant='outlined'>
+                        Create Game
+                    </Button>
+                }
+
+            </div>
+        </div>
+    );
+}
+
+const styles = {
+    app: {
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'stretch',
+        justifyContent: 'center',
+    },
+    debug: {
+        border: '1px solid red',
+        backgroundColor: 'lightgray',
+    },
+    container: {
+        flex: '1',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
 }
 
 export default App;
