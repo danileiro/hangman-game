bc77414be6937f8b97b56833dbbe4cc4f93c98f9
Danilo <danilo.rocha1@uni9.edu.br>
2021-06-08 23:11:08 -0300

    Abstract api requests

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,5 +3,28 @@ import { BASE_URL } from './config';
 const api = axios.create({
     baseURL: BASE_URL
 });
+export const createGame = (setTarget, setToken) => {
+    api.post("/hangman")
+        .then((res) => {
+            console.log(res);
+            setTarget(res.data.hangman);
+            setToken(res.data.token);
+        })
+        .catch((err) => console.log(err));
+}
 
-export default api;
+export const sendGuess = (token, guess, setResult) => {
+    api.put(`/hangman?token=${token}&letter=${guess}`)
+        .then((res) => {
+            setResult(res.data);
+        })
+        .catch((err) => console.log(err));
+}
+
+export const getTarget = (token, setTarget) => {
+    api.put(`/hangman?token=${token}`)
+        .then((res) => {
+            setTarget(res.data);
+        })
+        .catch((err) => console.log(err));
+}
