5a862c9b33d02475713e65b3507870b8a6be725c
Danilo <danilo.rocha1@uni9.edu.br>
2021-06-13 12:23:15 -0300

    Move controls to its own component

---
+++ src/components/Controls/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Button, TextField } from "@material-ui/core";
+
+const Controls = ({ isGameCreated, handleFormSubmit, guess, setGuess, handleCreateGame, handleGetHint }) => {
+    const handleInputChange = (e) => {
+        const { value } = e.target;
+        if (value.match(/^[a-zA-Z ]{0,1}$/)) {
+            setGuess(value);
+        }
+    }
+
+    return (
+        <div>
+            {isGameCreated ?
+                <>
+                    <form noValidate autoComplete='off' onSubmit={handleFormSubmit}>
+                        <TextField 
+                            label='Guess a letter'
+                            variant='outlined'
+                            value={guess}
+                            onChange={handleInputChange}
+                        />
+                    </form>
+                    {/* <Button onClick={handleGetHint} style={{margin:'5px'}} variant='outlined'>
+                        Hint?
+                    </Button> */}
+                </>
+            :
+                <Button onClick={handleCreateGame} variant='outlined'>
+                    Create Game
+                </Button>
+            }
+        </div>
+    )
+}
+
+export default Controls
